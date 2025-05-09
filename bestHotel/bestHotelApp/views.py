3d06b4c5b42f445c4681b45b4fa6227b20f50786
matthieuEv <evainmatthieu44@gmail.com>
2025-05-09 15:26:17 +0200

    fix: added better select for city

--- bestHotel/bestHotelApp/views.py
+++ bestHotel/bestHotelApp/views.py
@@ -6,8 +6,13 @@ def index(request):
     Render the index page of the bestHotel application.
     """
     cities = City.objects.all()
-    hotels = Hotel.objects.all()
+    selected_city = request.GET.get("city", "all")
+    if selected_city == "all":
+        hotels = Hotel.objects.all()
+    else:
+        hotels = Hotel.objects.filter(city__code=selected_city)
     return render(request, "bestHotelApp/index.html", {
         "cities": cities,
         "hotels": hotels,
+        "selected_city": selected_city,
     })
