bc418cf143af1c3487f90ab6caece46b1af247a4
matthieuEv <evainmatthieu44@gmail.com>
2025-05-09 17:08:28 +0200

    feat: added more style

--- bestHotel/bestHotelApp/views.py
+++ bestHotel/bestHotelApp/views.py
@@ -11,8 +11,14 @@ def index(request):
         hotels = Hotel.objects.all()
     else:
         hotels = Hotel.objects.filter(city__code=selected_city)
+    # Attribution d'une image statique à chaque hôtel (hotel1.jpg à hotel10.jpg)
+    hotels_with_images = []
+    for idx, hotel in enumerate(hotels):
+        image_number = (idx % 10) + 1
+        hotel.image_filename = f"bestHotelApp/images/hotel{image_number}.jpg"
+        hotels_with_images.append(hotel)
     return render(request, "bestHotelApp/index.html", {
         "cities": cities,
-        "hotels": hotels,
+        "hotels": hotels_with_images,
         "selected_city": selected_city,
     })
