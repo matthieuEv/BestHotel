4a69f6403df91b66d85f1a44780f9fa20bf29d56
matthieuEv <evainmatthieu44@gmail.com>
2025-05-09 14:21:13 +0200

    feat: first UI and model for city and hotels

--- bestHotel/bestHotelApp/models.py
+++ bestHotel/bestHotelApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+class City(models.Model):
+    code = models.CharField(
+        max_length=10,
+        primary_key=True,
+        help_text="Unique city code"
+    )
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.code} ({self.name})"
+
+
+class Hotel(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=255)
+    city = models.ForeignKey(
+        City,
+        db_column='city_code',
+        on_delete=models.CASCADE,
+        related_name='hotels'
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.city.code})"
 
--- bestHotel/bestHotelApp/views.py
+++ bestHotel/bestHotelApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 from .models import City, Hotel
 
+def index(request):
+    """
+    Render the index page of the bestHotel application.
+    """
+    cities = City.objects.all()
+    hotels = Hotel.objects.all()
+    return render(request, "bestHotelApp/index.html", {
+        "cities": cities,
+        "hotels": hotels,
+    })
